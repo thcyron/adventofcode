73fa17f1b3bee31af93c0e760f3deeb672f4c1e4
Thomas Cyron <thomas@thcyron.de>
2017-12-06 23:52:40 +0100

    Use idiomatic C++ code

--- 2017/day06.cpp
+++ 2017/day06.cpp
@@ -1,55 +1,48 @@
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <sstream>
 #include <vector>
 
-int index_to_rebalance(std::vector<int> config) {
-  int idx = 0, m = config[0];
-  for (int i = 1; i < config.size(); i++) {
-    if (config[i] > m) {
-      idx = i;
-      m = config[i];
-    }
-  }
-  return idx;
-}
+void rebalance(std::vector<int> &config) {
+  auto it = std::max_element(config.begin(), config.end());
+  auto idx = it - config.begin();
+  auto n = config[idx];
 
-std::vector<int> rebalance(std::vector<int> config) {
-  int idx = index_to_rebalance(config);
-  int n = config[idx];
   config[idx] = 0;
 
-  while (n > 0) {
+  while (n-- > 0) {
     idx = (idx + 1) % config.size();
     config[idx] += 1;
-    n--;
   }
-
-  return config;
 }
 
 int main() {
   std::string line;
   std::getline(std::cin, line);
 
+  std::vector<int> config;
+
   int n;
   std::istringstream iss(line);
-  std::vector<int> config;
   while (iss >> n) {
     config.push_back(n);
   }
 
-  std::map<std::vector<int>, int> seen_configs;
-  seen_configs[config] = 0;
+  std::map<std::vector<int>, int> seen_configs = {{config, 0}};
 
-  for (int rebalancings = 1;; rebalancings++) {
-    config = rebalance(config);
+  for (;;) {
+    rebalance(config);
+
+    auto s = seen_configs.size();
     auto it = seen_configs.find(config);
+
     if (it != seen_configs.end()) {
-      std::cout << rebalancings << std::endl;
-      std::cout << (rebalancings - it->second) << std::endl;
+      std::cout << s << std::endl;
+      std::cout << (s - it->second) << std::endl;
       break;
     }
-    seen_configs[config] = rebalancings;
+
+    seen_configs[config] = s;
   }
 }

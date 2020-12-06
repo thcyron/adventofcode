cc7b752e227c5b3ecf790e95736a1d1176cd3abe
Thomas Cyron <thomas@thcyron.de>
2020-12-06 06:27:47 +0100

    Simplify 2020-06

--- 2020/day06.py
+++ 2020/day06.py
@@ -1,6 +1,4 @@
-from functools import reduce
-
 with open("day06.txt") as f:
     groups = [[set(person) for person in group.split("\n")] for group in f.read().strip().split("\n\n")]
-    print(sum([len(answers) for group in groups for answers in reduce(set.union, group)]))
-    print(sum([len(answers) for group in groups for answers in reduce(set.intersection, group)]))
+    print(sum([len(answers) for group in groups for answers in set.union(*group)]))
+    print(sum([len(answers) for group in groups for answers in set.intersection(*group)]))
